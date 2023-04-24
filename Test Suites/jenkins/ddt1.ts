b6d8df5b039ba2ac0fa4d896b1a96e8701a4019b
sunil <messisunil@gmail.com>
2023-04-24 18:28:57 +0530

    added test case to ddt1

--- Test Suites/jenkins/ddt1.ts
+++ Test Suites/jenkins/ddt1.ts
@@ -5,10 +5,29 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>c30d7182-a7f9-4fd0-8532-d23b8c53b25a</testSuiteGuid>
+   <testCaseLink>
+      <guid>daa495d8-c618-4d9e-b256-f3c841dc7a85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DataDrivernTesting/ActiTime</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>f1c2d8c2-f18d-42d6-b832-1f6d2fcafcd3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>177d91da-d319-443d-b5d7-a330d69e24b5</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
