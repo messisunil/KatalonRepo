1c3c1696deea898c4fa0d6de54633e64972c9922
sunil <messisunil@gmail.com>
2023-04-24 18:37:21 +0530

    changed test case in ddt1

--- Test Suites/jenkins/ddt1.ts
+++ Test Suites/jenkins/ddt1.ts
@@ -12,22 +12,10 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>c30d7182-a7f9-4fd0-8532-d23b8c53b25a</testSuiteGuid>
    <testCaseLink>
-      <guid>daa495d8-c618-4d9e-b256-f3c841dc7a85</guid>
+      <guid>1cbf4436-9046-4dae-a780-c3b8efac0fb6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/DataDrivernTesting/ActiTime</testCaseId>
+      <testCaseId>Test Cases/Ebay/Filters</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>f1c2d8c2-f18d-42d6-b832-1f6d2fcafcd3</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>177d91da-d319-443d-b5d7-a330d69e24b5</variableId>
-      </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
